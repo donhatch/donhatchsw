0b0bafc3198e2dcb15a80c0e2cf32839ac9eb9cf
Don Hatch <dombiedoodle@gmail.com>
2006-12-28 19:40:03 +0000

    Initial revision

---
+++ MC4DApplet.php
@@ -0,0 +1,31 @@
+<html>
+	<body>
+                <!--
+		<body vlink="#99ff99" text="#ffcc99" link="#ccffff" bgcolor="#4d4d4d" alink="#ffcccc">
+                -->
+		<center><b><font size="+3">Magic Cube 4D Applet</font></b></center>
+		<applet 
+			archive="donhatchsw.jar" 
+			codebase="." 
+			code=com.donhatchsw.mc4d.MC4DViewApplet.class 
+			width="100%" height="80%"
+                        mayscript="true"
+                >
+			<PARAM NAME="puzzleDescription" VALUE="{5,3,3} 3">
+			<PARAM NAME="doDoubleBuffer" VALUE="true">
+		</applet>
+		Click a hypersticker to twist a hyperface.<br>
+
+		Left mouse twists counter-clockwise, right twists clockwise.<br>
+		Middle click (or Alt-click) 4D rotates a hyperface to the center.<br>
+		Dragging rotates the 3D view.
+                <!--
+		<center><p><font size="+1">
+			<a href="http://www.superliminal.com/cube/mc4dswing.jar">Download Full-Featured Application</a><br>
+			<a href="http://www.superliminal.com/cube/cube.htm">Return to the main MagicCube4D page</a><br>
+			<a href="http://www.superliminal.com">Return to the Superliminal home page</a>
+
+		</font></p></center>
+                -->
+	</body>
+</html>
