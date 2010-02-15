41819f574fde02495fabc70bf7da1d2803bef744
Don Hatch <dombiedoodle@gmail.com>
2010-02-15 21:37:17 +0000

    baby steps

--- com/donhatchsw/javacpp/javacpp.java
+++ com/donhatchsw/javacpp/javacpp.java
@@ -2,16 +2,24 @@ package com.donhatchsw.javacpp;
 
 public class javacpp
 {
+    private static void AssertAlways(boolean condition) { if (!condition) throw new Error("Assertion failed"); }
+
     public static void main(String args[])
     {
         Cpp1.ParsedCommandLineArgs parsedArgs = Cpp1.parseCommandLineArgs(args);
 
+        if (parsedArgs.inFileNames.length == 0)
+        {
+            System.err.println("Usage:");
+            System.err.println("    javacpp <<cpp options> <file>.prejava [<file1>.prejava ...]");
+            System.err.println("    javacpp <cpp options> javac <javac options> <file>.prejava [<file1>.prejava ...]");
+        }
         for (int i = 0; i < parsedArgs.inFileNames.length; ++i)
         {
             String inFileName = parsedArgs.inFileNames[i];
             if (!inFileName.endsWith(".prejava"))
             {
-                %ystem.err.println("Error: the input file \""+inFileName+"\" does not have the \".prejava\" extension.");
+                System.err.println("Error: the input file \""+inFileName+"\" does not have the \".prejava\" extension.");
                 System.exit(1);
             }
         }
@@ -20,9 +28,15 @@ public class javacpp
         {
             String prejavaFileName = parsedArgs.inFileNames[i];
             AssertAlways(prejavaFileName.endsWith(".prejava"));
-            String javaFileName = prejavaFileName.substring(prejavaFileName.length-".prejava".length);
-            System.err.println("    prejavaFileName = \""+prejavaFileName+"\");
-            System.err.println("       javaFileName = \""+javaFileName+"\");
+            String javaFileName = prejavaFileName.substring(0, prejavaFileName.length()-".prejava".length())+".java";
+            System.err.println("    prejavaFileName = \""+prejavaFileName+"\"");
+            System.err.println("       javaFileName = \""+javaFileName+"\"");
+
+            /*
+            java.io.Reader reader = 
+            open a reader to the filename
+            */
+
         }
     }
 } // class javacpp
