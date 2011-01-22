d56ab35f8a2d44e47e8f4f37ce67d37fd607e6f8
Don Hatch <dombiedoodle@gmail.com>
2011-01-22 22:27:32 +0000

    note about timestamp on manifest

--- com/donhatchsw/mc4d/PolytopePuzzleDescription.java
+++ com/donhatchsw/mc4d/PolytopePuzzleDescription.java
@@ -162,6 +162,7 @@
         - cascading menus don't cascade well, see if anything I can do (especially when hitting right side, or when trying to do it with clicks but not drags)
         - need good help describing all the controls, in the main help part
         - need to get javacpp fixed and usable before I ship this  (partially fixed now, still lame on jikes output)
+        - update date of MANIFEST.INF automatically
         - hotkeys don't work from java 1.6??  e.g. ctrl-c only gives what c gives
         - clicking on the < or > on side of the scrollbars only take about every other time
         - twist speed of generic 2x in melinda's is way too fast
