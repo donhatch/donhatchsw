donhatch/donhatchsw

commit ab62fc424f49e8c479e45be5aa316c78d4c32365

fix LT,GT and other macros

com/donhatchsw/applet/macros.h
@@ -12,11 +12,34 @@
 #define unimplemented() do {if (true) throw new Error("Unimplemented at "+__FILE__+"("+__LINE__+")"); } while (false)
 
 // NOTE: this causes a,b to be evaluated twice on failure, so is not ideal. might be better to do a block... ?
-#define assert_eq(a,b) do { if (!((a)==(b))) throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): (" + #a + ")==(" + #b + ") ("+(a)+" vs. "+(b)+")"); } while (false)
+#define assert_op(a,op,b) do { if (!((a)op(b))) throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): (" + #a + ")" + #op + "(" + #b + ") ("+(a)+" vs. "+(b)+")"); } while (false)
+#define assert_eq(a,b) assert_op(a,==,b)
+#define assert_le(a,b) assert_op(a,<=,b)
+#define assert_ge(a,b) assert_op(a,>=,b)
+#define assert_lt(a,b) assert_op(a,<,b)
+#define assert_gt(a,b) assert_op(a,>,b)
+#define assert_ne(a,b) assert_op(a,!=,b)
 #define assert_nan(x) do { if (!Double.isNaN(x)) throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): " + #x + " is "+(x)+", expected NaN"); } while (false)
 // Note: the additional "(a)==(b)||" is to make it work correctly for infinities...
 // and causes the args to be evaluated twice.
-#define assert_almost_eq(a,b,tol) do { if (!((a)==(b)||Math.abs((a)-(b))<=tol)) throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): (" + #a + ")==(" + #b + ")+-" + #tol + " ("+(a)+" vs. "+(b)+" +- "+(tol)+")"); } while (false)
+#define assert_almost_eq(a,b,tol) \
+    do { \
+        if (!((a)==(b)||Math.abs((a)-(b))<=tol)) \
+            throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): " \
+                           +"(" + #a + ")==(" + #b + ")+-" + #tol + "" \
+                           +" ("+(a)+" vs. "+(b)+" +- "+(tol)+")" \
+                           +" (error = "+((a)-(b))+")" \
+                           ); \
+    } while (false)
+#define assert_almost_inorder(a,b,c,tol) \
+    do { \
+        if (!((a)-(b)<=(tol)) || !((b)-(c)<=(tol))) \
+            throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): " \
+                           +"(" + #a + ")<=(" + #b + ")<=(" + #c + ")+-" + #tol + "" \
+                           +" ("+(a)+" vs. "+(b)+" vs. "+(c)+" +- "+(tol)+")" \
+                           +" (error = "+((a)-(b))+", "+((b)-(c))+")" \
+                           ); \
+    } while (false)
 
 #define INRANGE(foo,bar,baz) ((foo(bar))&&((bar)baz))
 
@@ -45,6 +68,7 @@
 #define PRINTARRAY_NONCOMPACT(x) System.out.println("        " + #x + " =\n" + Arrays.toStringNonCompact(x, "        ", "        "))
 
 #define ABS(x) ((x) < 0 ? -(x) : (x))
+#define SIGN(x) ((x) < 0 ? -1 : 1)
 #define HYPOTSQRD(a,b) (((a)*(a))+((b)*(b)))
 #define LERP(a,b,t) ((a) + (t)*((b)-(a)))
 #define SMOOTH(timeFrac) ((Math.sin(((timeFrac) - .5) * Math.PI) + 1) / 2)
@@ -71,10 +95,9 @@
 #define TRIANGLED(x) (((x)*((x)+1)) / 2)
 #define RTOD(r) ((r)*(180./Math.PI))
 #define DTOR(r) ((r)*(Math.PI/180.))
-
-#define LT(a,b,eps) (((b)-(a)) > (eps))
-#define GT(a,b,eps) (((a)-(b)) > (eps))
-#define LEQ(a,b,eps) (((a)-(b)) <= (eps))
-#define GEQ(a,b,eps) (((b)-(a)) <= (eps))
-#define EQ(a,b,eps) (LEQ(a,b,eps) && GEQ(a,b,eps))
+#define LT(a,b,tol) (((a)-(b)) < (tol))
+#define GT(a,b,tol) (((b)-(a)) < (tol))
+#define LEQ(a,b,tol) (((a)-(b)) <= (tol))
+#define GEQ(a,b,tol) (((b)-(a)) <= (tol))
+#define EQ(a,b,tol) (LEQ(a,b,tol) && GEQ(a,b,tol))
 #define MOD(a,b) (((a)+(b))%(b)) // XXX not general! only use it when a+b is known to be >= 0

com/donhatchsw/awt/macros.h
@@ -12,11 +12,34 @@
 #define unimplemented() do {if (true) throw new Error("Unimplemented at "+__FILE__+"("+__LINE__+")"); } while (false)
 
 // NOTE: this causes a,b to be evaluated twice on failure, so is not ideal. might be better to do a block... ?
-#define assert_eq(a,b) do { if (!((a)==(b))) throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): (" + #a + ")==(" + #b + ") ("+(a)+" vs. "+(b)+")"); } while (false)
+#define assert_op(a,op,b) do { if (!((a)op(b))) throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): (" + #a + ")" + #op + "(" + #b + ") ("+(a)+" vs. "+(b)+")"); } while (false)
+#define assert_eq(a,b) assert_op(a,==,b)
+#define assert_le(a,b) assert_op(a,<=,b)
+#define assert_ge(a,b) assert_op(a,>=,b)
+#define assert_lt(a,b) assert_op(a,<,b)
+#define assert_gt(a,b) assert_op(a,>,b)
+#define assert_ne(a,b) assert_op(a,!=,b)
 #define assert_nan(x) do { if (!Double.isNaN(x)) throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): " + #x + " is "+(x)+", expected NaN"); } while (false)
 // Note: the additional "(a)==(b)||" is to make it work correctly for infinities...
 // and causes the args to be evaluated twice.
-#define assert_almost_eq(a,b,tol) do { if (!((a)==(b)||Math.abs((a)-(b))<=tol)) throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): (" + #a + ")==(" + #b + ")+-" + #tol + " ("+(a)+" vs. "+(b)+" +- "+(tol)+")"); } while (false)
+#define assert_almost_eq(a,b,tol) \
+    do { \
+        if (!((a)==(b)||Math.abs((a)-(b))<=tol)) \
+            throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): " \
+                           +"(" + #a + ")==(" + #b + ")+-" + #tol + "" \
+                           +" ("+(a)+" vs. "+(b)+" +- "+(tol)+")" \
+                           +" (error = "+((a)-(b))+")" \
+                           ); \
+    } while (false)
+#define assert_almost_inorder(a,b,c,tol) \
+    do { \
+        if (!((a)-(b)<=(tol)) || !((b)-(c)<=(tol))) \
+            throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): " \
+                           +"(" + #a + ")<=(" + #b + ")<=(" + #c + ")+-" + #tol + "" \
+                           +" ("+(a)+" vs. "+(b)+" vs. "+(c)+" +- "+(tol)+")" \
+                           +" (error = "+((a)-(b))+", "+((b)-(c))+")" \
+                           ); \
+    } while (false)
 
 #define INRANGE(foo,bar,baz) ((foo(bar))&&((bar)baz))
 
@@ -45,6 +68,7 @@
 #define PRINTARRAY_NONCOMPACT(x) System.out.println("        " + #x + " =\n" + Arrays.toStringNonCompact(x, "        ", "        "))
 
 #define ABS(x) ((x) < 0 ? -(x) : (x))
+#define SIGN(x) ((x) < 0 ? -1 : 1)
 #define HYPOTSQRD(a,b) (((a)*(a))+((b)*(b)))
 #define LERP(a,b,t) ((a) + (t)*((b)-(a)))
 #define SMOOTH(timeFrac) ((Math.sin(((timeFrac) - .5) * Math.PI) + 1) / 2)
@@ -71,10 +95,9 @@
 #define TRIANGLED(x) (((x)*((x)+1)) / 2)
 #define RTOD(r) ((r)*(180./Math.PI))
 #define DTOR(r) ((r)*(Math.PI/180.))
-
-#define LT(a,b,eps) (((b)-(a)) > (eps))
-#define GT(a,b,eps) (((a)-(b)) > (eps))
-#define LEQ(a,b,eps) (((a)-(b)) <= (eps))
-#define GEQ(a,b,eps) (((b)-(a)) <= (eps))
-#define EQ(a,b,eps) (LEQ(a,b,eps) && GEQ(a,b,eps))
+#define LT(a,b,tol) (((a)-(b)) < (tol))
+#define GT(a,b,tol) (((b)-(a)) < (tol))
+#define LEQ(a,b,tol) (((a)-(b)) <= (tol))
+#define GEQ(a,b,tol) (((b)-(a)) <= (tol))
+#define EQ(a,b,tol) (LEQ(a,b,tol) && GEQ(a,b,tol))
 #define MOD(a,b) (((a)+(b))%(b)) // XXX not general! only use it when a+b is known to be >= 0

com/donhatchsw/compat/macros.h
@@ -12,11 +12,34 @@
 #define unimplemented() do {if (true) throw new Error("Unimplemented at "+__FILE__+"("+__LINE__+")"); } while (false)
 
 // NOTE: this causes a,b to be evaluated twice on failure, so is not ideal. might be better to do a block... ?
-#define assert_eq(a,b) do { if (!((a)==(b))) throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): (" + #a + ")==(" + #b + ") ("+(a)+" vs. "+(b)+")"); } while (false)
+#define assert_op(a,op,b) do { if (!((a)op(b))) throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): (" + #a + ")" + #op + "(" + #b + ") ("+(a)+" vs. "+(b)+")"); } while (false)
+#define assert_eq(a,b) assert_op(a,==,b)
+#define assert_le(a,b) assert_op(a,<=,b)
+#define assert_ge(a,b) assert_op(a,>=,b)
+#define assert_lt(a,b) assert_op(a,<,b)
+#define assert_gt(a,b) assert_op(a,>,b)
+#define assert_ne(a,b) assert_op(a,!=,b)
 #define assert_nan(x) do { if (!Double.isNaN(x)) throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): " + #x + " is "+(x)+", expected NaN"); } while (false)
 // Note: the additional "(a)==(b)||" is to make it work correctly for infinities...
 // and causes the args to be evaluated twice.
-#define assert_almost_eq(a,b,tol) do { if (!((a)==(b)||Math.abs((a)-(b))<=tol)) throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): (" + #a + ")==(" + #b + ")+-" + #tol + " ("+(a)+" vs. "+(b)+" +- "+(tol)+")"); } while (false)
+#define assert_almost_eq(a,b,tol) \
+    do { \
+        if (!((a)==(b)||Math.abs((a)-(b))<=tol)) \
+            throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): " \
+                           +"(" + #a + ")==(" + #b + ")+-" + #tol + "" \
+                           +" ("+(a)+" vs. "+(b)+" +- "+(tol)+")" \
+                           +" (error = "+((a)-(b))+")" \
+                           ); \
+    } while (false)
+#define assert_almost_inorder(a,b,c,tol) \
+    do { \
+        if (!((a)-(b)<=(tol)) || !((b)-(c)<=(tol))) \
+            throw new Error("Assertion failed at "+__FILE__+"("+__LINE__+"): " \
+                           +"(" + #a + ")<=(" + #b + ")<=(" + #c + ")+-" + #tol + "" \
+                           +" ("+(a)+" vs. "+(b)+" vs. "+(c)+" +- "+(tol)+")" \
+                           +" (error = "+((a)-(b))+", "+((b)-(c))+")" \
+                           ); \
+    } while (false)
 
 #define INRANGE(foo,bar,baz) ((foo(bar))&&((bar)baz))
 
@@ -45,6 +68,7 @@
 #define PRINTARRAY_NONCOMPACT(x) System.out.println("        " + #x + " =\n" + Arrays.toStringNonCompact(x, "        ", "        "))
 
 #define ABS(x) ((x) < 0 ? -(x) : (x))
+#define SIGN(x) ((x) < 0 ? -1 : 1)
 #define HYPOTSQRD(a,b) (((a)*(a))+((b)*(b)))
 #define LERP(a,b,t) ((a) + (t)*((b)-(a)))
 #define SMOOTH(timeFrac) ((Math.sin(((timeFrac) - .5) * Math.PI) + 1) / 2)
@@ -71,10 +95,9 @@
 #define TRIANGLED(x) (((x)*((x)+1)) / 2)
 #define RTOD(r) ((r)*(180./Math.PI))
 #define DTOR(r) ((r)*(Math.PI/180.))
-
-#define LT(a,b,eps) (((b)-(a)) > (eps))
-#define GT(a,b,eps) (((a)-(b)) > (eps))
-#define LEQ(a,b,eps) (((a)-(b)) <= (eps))
-#define GEQ(a,b,eps) (((b)-(a)) <= (eps))
-#define EQ(a,b,eps) (LEQ(a,b,eps) && GEQ(a,b,eps))
+#define LT(a,b,tol) (((a)-(b)) < (tol))
+#define GT(a,b,tol) (((b)-(a)) < (tol))
+#define LEQ(a,b,tol) (((a)-(b)) <= (tol))
+#define GEQ(a,b,tol) (((b)-(a)) <= (tol))
+#define EQ(a,b,tol) (LEQ(a,b,tol) && GEQ(a,b,tol))
 #define MOD(a,b) (((a)+(b))%(b)) // XXX not general! only use it when a+b is known to be >= 0

com/donhatchsw/util/macros.h
@@ -95,9 +95,8 @@
 #define TRIANGLED(x) (((x)*((x)+1)) / 2)
 #define RTOD(r) ((r)*(180./Math.PI))
 #define DTOR(r) ((r)*(Math.PI/180.))
-
-#define LT(a,b,tol) (((b)-(a)) > (tol))
-#define GT(a,b,tol) (((a)-(b)) > (tol))
+#define LT(a,b,tol) (((a)-(b)) < (tol))
+#define GT(a,b,tol) (((b)-(a)) < (tol))
 #define LEQ(a,b,tol) (((a)-(b)) <= (tol))
 #define GEQ(a,b,tol) (((b)-(a)) <= (tol))
 #define EQ(a,b,tol) (LEQ(a,b,tol) && GEQ(a,b,tol))

commit e880189f5650aca6b7756906e17010747b344dcf

Fix LT,GT which I apparently totally broke in ab62fc4

com/donhatchsw/applet/macros.h
@@ -95,8 +95,8 @@
 #define TRIANGLED(x) (((x)*((x)+1)) / 2)
 #define RTOD(r) ((r)*(180./Math.PI))
 #define DTOR(r) ((r)*(Math.PI/180.))
-#define LT(a,b,tol) (((a)-(b)) < (tol))
-#define GT(a,b,tol) (((b)-(a)) < (tol))
+#define LT(a,b,tol) (((b)-(a)) > (tol))
+#define GT(a,b,tol) (((a)-(b)) > (tol))
 #define LEQ(a,b,tol) (((a)-(b)) <= (tol))
 #define GEQ(a,b,tol) (((b)-(a)) <= (tol))
 #define EQ(a,b,tol) (LEQ(a,b,tol) && GEQ(a,b,tol))

com/donhatchsw/awt/macros.h
@@ -95,8 +95,8 @@
 #define TRIANGLED(x) (((x)*((x)+1)) / 2)
 #define RTOD(r) ((r)*(180./Math.PI))
 #define DTOR(r) ((r)*(Math.PI/180.))
-#define LT(a,b,tol) (((a)-(b)) < (tol))
-#define GT(a,b,tol) (((b)-(a)) < (tol))
+#define LT(a,b,tol) (((b)-(a)) > (tol))
+#define GT(a,b,tol) (((a)-(b)) > (tol))
 #define LEQ(a,b,tol) (((a)-(b)) <= (tol))
 #define GEQ(a,b,tol) (((b)-(a)) <= (tol))
 #define EQ(a,b,tol) (LEQ(a,b,tol) && GEQ(a,b,tol))

com/donhatchsw/compat/macros.h
@@ -95,8 +95,8 @@
 #define TRIANGLED(x) (((x)*((x)+1)) / 2)
 #define RTOD(r) ((r)*(180./Math.PI))
 #define DTOR(r) ((r)*(Math.PI/180.))
-#define LT(a,b,tol) (((a)-(b)) < (tol))
-#define GT(a,b,tol) (((b)-(a)) < (tol))
+#define LT(a,b,tol) (((b)-(a)) > (tol))
+#define GT(a,b,tol) (((a)-(b)) > (tol))
 #define LEQ(a,b,tol) (((a)-(b)) <= (tol))
 #define GEQ(a,b,tol) (((b)-(a)) <= (tol))
 #define EQ(a,b,tol) (LEQ(a,b,tol) && GEQ(a,b,tol))

com/donhatchsw/util/macros.h
@@ -95,8 +95,8 @@
 #define TRIANGLED(x) (((x)*((x)+1)) / 2)
 #define RTOD(r) ((r)*(180./Math.PI))
 #define DTOR(r) ((r)*(Math.PI/180.))
-#define LT(a,b,tol) (((a)-(b)) < (tol))
-#define GT(a,b,tol) (((b)-(a)) < (tol))
+#define LT(a,b,tol) (((b)-(a)) > (tol))
+#define GT(a,b,tol) (((a)-(b)) > (tol))
 #define LEQ(a,b,tol) (((a)-(b)) <= (tol))
 #define GEQ(a,b,tol) (((b)-(a)) <= (tol))
 #define EQ(a,b,tol) (LEQ(a,b,tol) && GEQ(a,b,tol))