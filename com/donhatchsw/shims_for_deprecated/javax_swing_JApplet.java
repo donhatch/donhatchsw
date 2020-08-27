05056d6277b4ef915dce9b520d0acbf8e34f6486
Don Hatch <hatch@plunk.org>
2020-08-27 16:35:44 -0700

    Update all precompiled java files after javacpp behavior cleanup.
    javacpp now adds -ffreestanding to the cpp command line flags,
    resulting in much less cluttered and less churn-prone java files.
    This updates all the precompiled .java files in the java6
    (currently same as java7..11) branches to be the new cleaner output.

--- com/donhatchsw/shims_for_deprecated/javax_swing_JApplet.java
+++ com/donhatchsw/shims_for_deprecated/javax_swing_JApplet.java
@@ -101,66 +101,19 @@
 // 102 # 1 "com/donhatchsw/shims_for_deprecated/javax_swing_JApplet.prejava"
 // 103 # 1 "<built-in>"
 // 104 # 1 "<command-line>"
-// 105 # 1 "/usr/include/stdc-predef.h" 1 3 4
-
-// 107 # 1 "/usr/include/stdc-predef.h" 3 4
-/* Copyright (C) 1991-2019 Free Software Foundation, Inc.
-   This file is part of the GNU C Library.
-
-   The GNU C Library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public
-   License as published by the Free Software Foundation; either
-   version 2.1 of the License, or (at your option) any later version.
-
-   The GNU C Library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library; if not, see
-   <http://www.gnu.org/licenses/>.  */
-
-
-
-
-/* This header is separate from features.h so that the compiler can
-   include it implicitly at the start of every compilation.  It must
-   not itself include <features.h> or any other header that includes
-   <features.h> because the implicit include comes before any feature
-   test macros that may be defined in a source file before it first
-   explicitly includes a system header.  GCC knows the name of this
-   header in order to preinclude it.  */
-
-/* glibc's intent is to support the IEC 559 math functionality, real
-   and complex.  If the GCC (4.9 and later) predefined macros
-   specifying compiler intent are available, use them to determine
-   whether the overall intent is to support these features; otherwise,
-   presume an older compiler has intent to support these features and
-   define these macros by default.  */
-// 142 # 52 "/usr/include/stdc-predef.h" 3 4
-/* wchar_t uses Unicode 10.0.0.  Version 10.0 of the Unicode Standard is
-   synchronized with ISO/IEC 10646:2017, fifth edition, plus
-   the following additions from Amendment 1 to the fifth edition:
-   - 56 emoji characters
-   - 285 hentaigana
-   - 3 additional Zanabazar Square characters */
-// 149 # 1 "<command-line>" 2
-// 150 # 1 "com/donhatchsw/shims_for_deprecated/javax_swing_JApplet.prejava"
-// 151 # 1 "com/donhatchsw/shims_for_deprecated/macros.h" 1
-
-// 153 # 1 "com/donhatchsw/shims_for_deprecated/macros.h"
+// 105 # 1 "com/donhatchsw/shims_for_deprecated/javax_swing_JApplet.prejava"
+// 106 # 1 "com/donhatchsw/shims_for_deprecated/macros.h" 1
 //
 // macros.h
 //
-// 157 # 14 "com/donhatchsw/shims_for_deprecated/macros.h"
+// 110 # 14 "com/donhatchsw/shims_for_deprecated/macros.h"
 // NOTE: this causes a,b to be evaluated twice on failure, so is not ideal. might be better to do a block... ?
-// 159 # 30 "com/donhatchsw/shims_for_deprecated/macros.h"
+// 112 # 30 "com/donhatchsw/shims_for_deprecated/macros.h"
 // Note: the additional "(a)==(b)||" is to make it work correctly for infinities...
 // and causes the args to be evaluated twice.
-// 162 # 67 "com/donhatchsw/shims_for_deprecated/macros.h"
+// 115 # 67 "com/donhatchsw/shims_for_deprecated/macros.h"
 // XXX ... what a mess
-// 164 # 2 "com/donhatchsw/shims_for_deprecated/javax_swing_JApplet.prejava" 2
+// 117 # 2 "com/donhatchsw/shims_for_deprecated/javax_swing_JApplet.prejava" 2
 
 package com.donhatchsw.shims_for_deprecated;
 
